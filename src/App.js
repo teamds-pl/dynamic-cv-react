bfebec2af94925167bf678c8e06a634c5228f701
Dawid Kozak <dawid.kozak@ds-consultants.eu>
2018-04-25 13:50:37 +0200

    add mobx

--- src/App.js
+++ src/App.js
@@ -4,79 +4,69 @@ import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 import './App.css';
 import Cv from "./cv/Cv";
-import firebase from './firebase';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
+import { observer } from 'mobx-react'; 
+import UserStore from './stores/userStore';  
 
-    this.usersRef = firebase.database().ref('users');
+const App = observer(
+  class App extends Component {
+    constructor(props) {
+      super(props)
 
-    this.state = {
-      value: 'select',
-      users: []
-    }
-  }
-
-  componentDidMount() {
-    this.usersRef.on('value', (snapshot) => {
-      let users = snapshot.val();
-      let newState = [];
-      for (let user in users) {
-        newState.push(users[user]);
+      this.state = {
+        value: 'select',
+        users: []
       }
-      this.setState({
-        users: newState
-      });
-    });
-  }
-
-  printDocument() {
-    const input = document.getElementById('divToPrintPage1');
-    const input2 = document.getElementById('divToPrintPage2');
-    html2canvas(input).then((canvas) => {
-      html2canvas(input2).then((canvas2) => {
-        const imgData = canvas.toDataURL('image/png');
-        const imgData2 = canvas2.toDataURL('image/png');
-        const pdf = new jsPDF();
-        pdf.addImage(imgData, 'JPEG', 0, 0);
-        pdf.addPage();
-        pdf.addImage(imgData2, 'JPEG', 0, 0);
-        pdf.save("download.pdf");
-      });
-    });
-  }
-
-  change(event) {
-    this.setState({
-      value: event.target.value
-    });
-  }
-
-  render() {
-    let user = {};
-    if (this.state.users[0]) {
-      user = this.state.users[0];
     }
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logoDS} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Dynamic CV</h1>
-        </header>
-        <p className="App-intro">
-          <button onClick={this.printDocument}>Print</button>
-          <select id="lang" onChange={this.change} value={this.state.value}>
-            <option value="select">Select</option>
-            <option value="Java">Java</option>
-            <option value="C++">C++</option>
-          </select>
-        </p>
-        <Cv user={user} />
-      </div>
-    );
+    printDocument() {
+      const input = document.getElementById('divToPrintPage1');
+      const input2 = document.getElementById('divToPrintPage2');
+      html2canvas(input).then((canvas) => {
+        html2canvas(input2).then((canvas2) => {
+          const imgData = canvas.toDataURL('image/png');
+          const imgData2 = canvas2.toDataURL('image/png');
+          const pdf = new jsPDF();
+          pdf.addImage(imgData, 'JPEG', 0, 0);
+          pdf.addPage();
+          pdf.addImage(imgData2, 'JPEG', 0, 0);
+          pdf.save("download.pdf");
+        });
+      });
+    }
+
+    change(event) {
+      this.setState({
+        value: event.target.value
+      });
+    }
+
+    render() {
+      let users = UserStore.allUsers;
+      let user = {};
+      if (users[0]) {
+        user = users[0];
+      }
+
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logoDS} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to Dynamic CV</h1>
+          </header>
+          <p className="App-intro">
+            <button onClick={this.printDocument}>Print</button>
+            <select id="lang" onChange={this.change} value={this.state.value}>
+              <option value="select">Select</option>
+              <option value="Java">Java</option>
+              <option value="C++">C++</option>
+            </select>
+          </p>
+          <Cv user={user} />
+        </div>
+      );
+    }
   }
-}
+)
 
 export default App;
---
+++ src/stores/userStore.js
@@ -0,0 +1,31 @@
+import { observable, decorate, computed } from "mobx"
+import firebase from '../firebase';
+
+class UserStore {
+    users = [];
+    usersRef;
+
+    get allUsers(){
+        return this.users.slice();
+    }
+
+    constructor() {
+        this.usersRef = firebase.database().ref('users');
+
+        this.usersRef.on('value', (snapshot) => {
+            let users = snapshot.val();
+            let newState = [];
+            for (let user in users) {
+              newState.push(users[user]);
+            }
+            this.users = newState;
+        });
+    }
+}
+
+decorate(UserStore, {
+    users: observable,
+    allUsers: computed
+})
+
+export default new UserStore();
